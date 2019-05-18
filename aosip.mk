160580cb08f6122193457aa3512257c32d3f10a2
Kesav <49778131+kesavvvvv@users.noreply.github.com>
2019-05-18 05:30:44 +0530

    Aosip Bringup

---
+++ aosip.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00T
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := aosip_X00T
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase.ms=android-asus-tpin
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys
+
+# The following system and vendor props will be set by vendor init
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
+    ro.product.device \
+    ro.product.model \
+    ro.product.name
+
+PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
+    ro.vendor.product.device \
+    ro.vendor.product.model \
+    ro.vendor.product.name
